4e50a45d0b9ea15624906df257b68620de1144ff
JGachancipa <150005302+JGachancipa@users.noreply.github.com>
2024-11-28 18:33:13 -0500

    Update AuthProvider.tsx

--- frontend/web/src/context/AuthContext/AuthProvider.tsx
+++ frontend/web/src/context/AuthContext/AuthProvider.tsx
@@ -21,6 +21,7 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify(credentials),
+                    credentials: 'include'
                 });
 
                 if (!response.ok) {
