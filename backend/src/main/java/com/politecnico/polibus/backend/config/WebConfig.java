f0e506e6b8f42a4ea4d5a604d3b684ebae9a5b99
JGachancipa <150005302+JGachancipa@users.noreply.github.com>
2024-11-28 18:42:30 -0500

    Update WebConfig.java

--- backend/src/main/java/com/politecnico/polibus/backend/config/WebConfig.java
+++ backend/src/main/java/com/politecnico/polibus/backend/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.politecnico.operacionales.api.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,15 +9,30 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     private static final String ALLOWED_ORIGINS = "https://polibus-front-b2195b1b644b.herokuapp.com";
-    private static final String[] ALLOWED_METHODS = {"GET", "POST", "OPTIONS"};
+    private static final String[] ALLOWED_METHODS = {"GET", "POST"};
     private static final String[] ALLOWED_HEADERS = {"Authorization", "Content-Type", "Accept"};
-
+    
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins(ALLOWED_ORIGINS)
                 .allowedMethods(ALLOWED_METHODS)
                 .allowedHeaders(ALLOWED_HEADERS)
-                .allowCredentials(true);
+                .allowCredentials(true)  // Asegura que las credenciales (cookies, Authorization) se permitan
+                .exposedHeaders("Authorization"); // Expone el encabezado Authorization, si es necesario
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(ALLOWED_ORIGINS)
+                        .allowedMethods(ALLOWED_METHODS)
+                        .allowedHeaders(ALLOWED_HEADERS)
+                        .allowCredentials(true);
+            }
+        };
     }
 }
